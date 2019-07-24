685e15c9de0c72ee66b448107e55911163fd69f3
Michael Lawrence <michafla@gene.com>
2019-07-24 14:47:05 -0700

    create 'stach' script

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': ['smux=smux.__main__:main' ],
+        'console_scripts': ['stach=stach.__main__:main' ],
         'gui_scripts': []
     },
 )
